1ef4db340683fc46529c02e397454cb1d4b4adb2
ryosukes <nu.ciger@gmail.com>
2018-08-20 21:44:46 +0900

    init firebase

--- src/main.js
+++ src/main.js
@@ -3,13 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+const config = {
+  apiKey: 'API_KEY',
+  authDomain: 'DOMAIN.firebaseapp.com',
+  databaseURL: 'DOMAIN.firebaseio.com',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'BUCKET_ID.appspot.com',
+  messagingSenderId: 'SENDER_ID'
+}
+firebase.initializeApp(config)
+
+firebase.auth().onAuthStateChanged(user => {
+  /* eslint-disable no-new */
+  if (!app) {
+    new Vue({
+      el: '#app',
+      router,
+      components: { App },
+      template: '<App/>'
+    })
+  }
 })
